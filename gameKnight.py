3bdc9d136284e378c6b15fa11179bbb999bba77e
REAPER\Josh <j.painter@digipen.edu>
2019-03-26 20:38:19 -0700

    got rid of private key file

--- gameKnight.py
+++ gameKnight.py
@@ -1,11 +1,18 @@
+#!/usr/bin/python
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-client = gspread.authorize(creds)
+def main():
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('Z:\GameKnight\client_secret.json', scope)
+    client = gspread.authorize(creds)
 
-sheet = client.open('Game Knight').sheet1
+    sheet = client.open('Game Knight').sheet1
 
-records = sheet.get_all_records()
-print(records)
+    #records = sheet.get_all_records()
+    print("got here fam")
+    val = sheet.acell('B1').value
+    print(val)
+
+if __name__ == "__main__":
+    main()
