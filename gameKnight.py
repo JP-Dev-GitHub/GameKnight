4f0bf56f317783b6125292f94a24c10692de4c1c
Mini Reaper <joshxpainter@gmail.com>
2019-03-26 19:02:48 -0700

    i luv it

---
+++ gameKnight.py
@@ -0,0 +1,11 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+client = gspread.authorize(creds)
+
+sheet = client.open('Game Knight').sheet1
+
+records = sheet.get_all_records()
+print(records)
