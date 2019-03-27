3bb753abfb5bb19225fc44fca82ce720e348c031
Josh <josh.painter@avirtek.com>
2019-03-27 11:03:14 -0700

    how do I remove this damn thing?

--- gameKnight.py
+++ gameKnight.py
@@ -2,15 +2,17 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
+SECRET_PATH = '/home/josh/Desktop/gameknight/GameKnight/client_secret.json'
+
+
 def main():
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_name('Z:\GameKnight\client_secret.json', scope)
+    creds = ServiceAccountCredentials.from_json_keyfile_name(SECRET_PATH, scope)
     client = gspread.authorize(creds)
 
     sheet = client.open('Game Knight').sheet1
 
     #records = sheet.get_all_records()
-    print("got here fam")
     val = sheet.acell('B1').value
     print(val)
 
